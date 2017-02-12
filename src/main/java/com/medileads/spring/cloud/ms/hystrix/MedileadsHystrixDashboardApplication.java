8f26dc45431d87df2818ee61a900517e2dc190b6
Harinath <harinathk7@gmail.com>
2017-02-12 20:13:01 +0800

    initial commit

---
+++ src/main/java/com/medileads/spring/cloud/ms/hystrix/MedileadsHystrixDashboardApplication.java
@@ -0,0 +1,16 @@
+package com.medileads.spring.cloud.ms.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableTurbine
+public class MedileadsHystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedileadsHystrixDashboardApplication.class, args);
+	}
+}
